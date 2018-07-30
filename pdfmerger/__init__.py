3d894252f48694b3fdfd8219f3636d8460dc1c47
Murray Christopherson <murraychristopherson@gmail.com>
2018-07-30 17:18:48 -0400

    Working around an issue with wkhtmltopdf generating non-readable PDFs

--- pdfmerger/__init__.py
+++ pdfmerger/__init__.py
@@ -1,10 +1,10 @@
-from PyPDF2 import PdfFileMerger
+from PyPDF2 import PdfFileReader, PdfFileMerger
 
 
-def merge(input_file_objs, output_file_obj):
+def merge(input_file_objs, output_file_obj, _pypdf2_import_bookmarks=False):
     merger = PdfFileMerger()
     for file_obj in input_file_objs:
-        merger.append(file_obj)
+        merger.append(file_obj, import_bookmarks=_pypdf2_import_bookmarks)
 
     merger.write(output_file_obj)
 
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='pdfmerger',
-    version='0.3.0',
+    version='0.4.0',
     description='Merge PDFs (inspired by metaist/pdfmerge)',
     long_description=long_description,
     long_description_content_type='text/markdown',
